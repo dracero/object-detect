17f349ce290935139e2a6717ff5af9741a9aa29c
dracero <diego.racero@gmail.com>
2023-05-06 18:53:17 -0300

    vercel8

--- src/hook.js
+++ src/hook.js
@@ -1,22 +1,6 @@
-import { registerSW } from 'svelte-kit/sw';
-import { Workbox } from 'workbox-window';
-import { onMount } from 'svelte';
+import { dev } from '$app/environment';
 
-import './service-worker.js';
-
-const workbox = new Workbox('./service-worker.js');
-
-registerSW({
-  onNeedRefresh() {
-    workbox.addEventListener('controlling', () => {
-      window.location.reload();
-    });
-
-    workbox.messageSkipWaiting();
-  },
-});
-
-onMount(() => {
-  workbox.register();
+navigator.serviceWorker.register('/service-worker.js', {
+    type: dev ? 'module' : 'classic'
 });
 

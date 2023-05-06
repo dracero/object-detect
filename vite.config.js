2edef87d13d5a3fc6131826f4cf02769f8396e66
dracero <diego.racero@gmail.com>
2023-05-06 19:52:23 -0300

    test3

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit'
+import { sveltekit } from "@sveltejs/kit/vite"
 
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			srcDir: 'src',
-			filename: 'service-worker.js', 
-			// or `my-sw.ts`
-			/* other pwa options */  
-		  }),
-	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+}
+
+export default config
+
+

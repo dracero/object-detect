934107fa2b3c5fe04becbf11a36fc410536e0864
dracero <diego.racero@gmail.com>
2023-05-06 18:25:48 -0300

    vercel4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,6 @@
 <script>
-    import { onMount } from 'svelte';
-    import { dev } from '$app/environment';
+    
     import Cam from './Cam.svelte';
-   
-    function registerServiceWorker() {
-        if ('serviceWorker' in navigator) {
-            addEventListener('load', function () {
-                navigator.serviceWorker.register('../service-worker.js');
-    });
-}
-    }
-
-   // Detecting service worker update and reload the page.
-    async function detectSWUpdate() {
-          const registration = await navigator.serviceWorker.ready;
-          registration.addEventListener("updatefound", event => {
-            const newSW = registration.installing;
-            newSW.addEventListener("statechange", event => {
-                if (newSW.state == "installed") {
-                    console.log('New service worker installed');
-                    // New service worker is installed, but waiting activation
-      }
-    });
-  })
-}
-
-
-    onMount(async () => {   
-        registerServiceWorker();
-        detectSWUpdate();
-    });
 
 </script>
 <Cam />

fd9013a15eed529c8f8ce9caf12b43bc366e9485
dracero <dracero>
2023-06-03 20:41:56 -0300

    screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,12 @@
 	//import Cam from './Cam.svelte';
     import Detect from './Detec.svelte';
     import Snap from './Snap.svelte';
+    import Screen from './Screen.svelte';
 
 </script>
 <Detect />
 <Snap />
+<Screen />
+
 
 
---
+++ src/routes/Screen.svelte
@@ -0,0 +1,16 @@
+<script>
+    // @ts-nocheck
+    import html2canvas from 'html2canvas';
+  
+    let screenshotUrl = '';
+  
+    function takeScreenshot() {
+      html2canvas(document.body).then(canvas => {
+        screenshotUrl = canvas.toDataURL();
+        console.log(screenshotUrl);
+      });
+    }
+  </script>
+  
+  <button on:click={takeScreenshot}>Take Screenshot</button>
+  

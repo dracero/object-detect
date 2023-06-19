3e60290068b5dc976a0a1574582fa9587764767c
dracero <dracero>
2023-06-19 19:50:14 -0300

    uth

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -20,8 +20,7 @@
 <Speech />
 <Detectab />
 <Mic />
-<!--<iframe src="http://chimuelo.fi.uba.ar/" title="Google" width="100%" height="500"></iframe>
--->
+<iframe src="http://chimuelo.fi.uba.ar/" title="Google" width="100%" height="500"></iframe>
 <p>Session expiry: {$page.data.session?.expires}</p>
 {:else}
 <h1>Access Denied</h1>
